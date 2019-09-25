db7a49aa309ec8e0e4c5e38663f983f6ba60ad7e
Minh Duc <ducc.minh.ngn@gmail.com>
2019-09-25 22:14:20 +0700

    Code moi

---
+++ Session2_var/var_js.js
@@ -0,0 +1,31 @@
+// console.log("Hello, world!");
+// console.log("Nguyen_Minh_Duc");
+// console.log("19");
+// console.log("Ha_Noi");
+// console.log("Fan_Faker!!");
+// let name = "Faker"; //Khai bao va gan gia tri
+// console.log(name);
+
+// var age = 20;
+// console.log(age);
+
+// let a;
+// console.log(a);
+
+// const pi = 3434334;
+// console.log(pi);
+
+// pi = 3.14;
+// console.log(pi);
+
+let a = 5;
+console.log(a);
+a = 10;
+console.log(a);
+
+
+
+
+
+
+
