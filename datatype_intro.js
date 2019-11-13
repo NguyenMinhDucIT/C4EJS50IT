0dbba2709b918f0dc6792b790f9b18ac0dd76fe0
NguyenMinhDucIT <55796092+NguyenMinhDucIT@users.noreply.github.com>
2019-11-13 21:57:33 +0700

    Add files via upload

---
+++ datatype_intro.js
@@ -0,0 +1,31 @@
+// let a = 20;
+// let name = "abc";
+
+// console.log(typeof a);
+// console.log(typeof name);
+
+// let pi = 3.14;
+// console.log( typeof pi);
+
+// let r = parseFloat('5');
+// let age = parseInt('5.5');
+// console.log(r);
+// console.log(age);
+
+// let b = a + name; //số + chuỗi
+// let c = "xin chào" + name; //chuỗi + chuỗi
+// let d = name + a; //chuỗi + số
+
+// // let e = "abc" - "def"
+// // console.log(e); // sẽ ra NaN not a number
+
+// let e = Number("200") - Number("100");
+// console.log(e); // ra số
+
+// let name = prompt("Bạn tên là gì?");
+// console.log(name);
+// // let tuoi = prompt("Bạn bao nhiêu tuổi?");
+// // console.log(tuoi);
+// let year = parseInt(prompt("Năm sinh: "));
+// let age = 2019 - year;
+// console.log(`xin chào: ${name}, bạn ${age} tuổi`);
